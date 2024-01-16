b62e852194f51d91cadb0dd7b797ef74b6296861
Ridwan <ridhwan.alabi@gmail.com>
2024-01-16 13:38:13 +0100

    Complete about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,17 +7,19 @@ import AboutImage from "./aboutImage.png"
 export default function About() {
   return (
     <section className={`${classes.aboutWrap} d-flex justify-content-between`}>
-      <div>
+      <div className="col-5">
         <img src={AboutImage} alt="About Image" />
       </div>
-      <div>
-        <div>
+      <div className={`${classes.aboutContent} d-flex`}>
+        <div className={classes.header}>
           <h3>We Love Cheesecake</h3>
           <p>Discover Our Cheesecake Story</p>
         </div>
         <p>At Cheesecake Love, quality is our cornerstone. Each cheesecake is crafted with the finest ingredients, meticulous attention to detail, and a dash of creativity.</p>
         <p>We believe in delivering not just desserts but moments of sheer indulgence.</p>
-        <TransparentButton name="Read More" darkButton={true} />
+        <div>
+          <TransparentButton name="Read More" darkButton={true} />
+        </div>
       </div>
     </section>
   )
